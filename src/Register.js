d999b4aa4b4259bfd055490b1bf7e04ff560579d
ihor-vip <vip20140902@gmail.com>
2023-02-11 13:11:36 +0200

    moving user input in form

--- src/Register.js
+++ src/Register.js
@@ -57,19 +57,19 @@ const Register = () => {
       <h1>Register</h1>
       <form>
         <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          required
+          aria-invalid={validName ? "false" : "true"}
+          aria-describedby="uidnote"
+          onFocus={() => setUserFocus(true)}
+          onBlur={() => setUserFocus(false)}
+        />
       </form>
-      <input
-        type="text"
-        id="username"
-        ref={userRef}
-        autoComplete="off"
-        onChange={(e) => setUser(e.target.value)}
-        required
-        aria-invalid={validName ? "false" : "true"}
-        aria-describedby="uidnote"
-        onFocus={() => setUserFocus(true)}
-        onBlur={() => setUserFocus(false)}
-      />
     </section>
   );
 };
